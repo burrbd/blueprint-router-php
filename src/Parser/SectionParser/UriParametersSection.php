d5541a58b55985d0c4a36dd8f07082d4cc8b743d
Bay Dodd <baydodd@gmail.com>
2016-06-23 15:39:23 +0100

    Add initial URI parameters section

---
+++ src/Parser/SectionParser/UriParametersSection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BlueprintRouter\Parser\SectionParser;
+
+class UriParametersSection implements SectionParser
+{
+    /**
+     * @param string   $content
+     * @param resource $handle
+     *
+     * @return \BlueprintRouter\Endpoint\Definition|null
+     */
+    public function parseDefinition($content, $handle)
+    {
+        // TODO: Implement parseDefinition() method.
+    }
+}
